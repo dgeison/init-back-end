aa3b295bfa0efdf876da2b838444a0686746cb47
Dgeison <paradg@gmail.com>
2021-01-12 15:59:44 -0300

    inserimos um arquivo data e indicamos ids

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: "7vSwoy63cy0",
+    title: "Universo GO - #NeverStopLearning",
+    duration: "57 min",
+    price: "Free",
+  },
+  {
+    id: "TtZfCLViuLo",
+    title: "Universo Swift - #NeverStopLearning",
+    duration: "57 min",
+    price: "Free",
+  },
+  {
+    id: "vKazFN3nbhs",
+    title: "Universo Kotlin - #NeverStopLearning",
+    duration: "57 min",
+    price: "Free",
+  },
+  {
+    id: "ZOdt0PAlhas",
+    title: "Universo Ruby - #NeverStopLearning",
+    duration: "57 min",
+    price: "Free",
+  },
+  {
+    id: "BVWdPuNjuiY",
+    title: "Universo Java - #NeverStopLearning",
+    duration: "57 min",
+    price: "Free",
+  },
+  {
+    id: "8asV5hADN78",
+    title: "Universo Elixir - #NeverStopLearning",
+    duration: "57 min",
+    price: "Free",
+  },
+];
